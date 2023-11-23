9cb151685ddfdccb7f75746be2a01fd6e309de17
Kevin <minuscoca@gmail.com>
2023-11-23 22:06:30 +0800

    create getValue util type

--- index.ts
+++
@@ -1,3 +0,0 @@
-import { removeDuplicates } from './leetcode/26/remove-duplicates-from-sorted-array';
-
-removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]);
---
+++ libs/utils.ts
@@ -0,0 +1,3 @@
+export function getValue<T, K extends keyof T>(obj: T, key: K) {
+  return obj[key];
+}
